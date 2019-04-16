15c886b557a94d020f31b90c14426b8442fc86ae
phuchung <nphbo1998@gmail.com>
2019-04-17 01:28:41 +0700

    create modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Modal from './Components'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h3>Hello word</h3>
+        <h3>This is modal</h3>
+        <Modal/>
       </div>
     );
   }
---
+++ src/Components/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Modal from './Modals';
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <Modal/>
+      </div>
+    )
+  }
+}
